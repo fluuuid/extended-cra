f832a67912c993e68f71c4d808adb44522e915a1
Silvio Paganini <silvio@fluuu.id>
2020-02-14 10:50:24 +0000

    background black

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -74,6 +74,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     margin: 0;
     padding: 0;
+    background-color: black;
   }
 `;
 
